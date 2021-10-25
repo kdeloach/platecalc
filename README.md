80bbf9178a6a9eac35c002fa9945e833c7ce65a5
Kevin DeLoach <kdeloach@gmail.com>
2021-10-25 15:06:23 -0400

    First draft

--- README.md
+++ README.md
@@ -1,2 +1,94 @@
 # platecalc
-Generate efficient workout plans for weightlifting barbells
+
+Command line utility to generate optimal workout plans for weightlifting
+barbells.
+
+The `calc` command outputs which plates to use and the optimal order to load
+them on the bar to minimize effort across a series of target weights.
+
+The `plan` command outputs a workout plan based on Jim Wendler's 5/3/1 BBB
+program in CSV format.
+
+## Commands
+
+### calc
+
+Calculate which plates to use and the order to load them on the bar. The
+order is optimized to minimize effort to change plates during a workout.
+
+Usage:
+
+```sh
+$ go run ./cmd/calc/ -h
+Usage: calc [weight:int]+
+  -bar int
+        bar weight (default 45)
+  -plates string
+        available plates (default "45,35,25,10,10,5,5,2.5")
+```
+
+Example to calculate plates needed to lift `110`:
+
+```sh
+$ go run ./cmd/calc/ 110
+110: 25, 5, 2.5  # 110 = (25+5+2.5)*2 + 45(bar)
+```
+
+Notice how the plate order for `110` shifts if it's preceded by `100` so you
+only need to add 1 plate instead of removing 2 then adding 1:
+
+```sh
+$ go run ./cmd/calc/ 100 110
+100: 25, 2.5
+110: 25, 2.5, 5
+```
+
+Example FSL (first-set-last) workout sets:
+
+```sh
+$ go run ./cmd/calc/ 210 240 265 210
+210: 45, 35, 2.5
+240: 45, 35, 2.5, 10, 5
+265: 45, 35, 25, 5
+210: 45, 35, 2.5
+```
+
+### plan
+
+Generate workout plan based on Jim Wendler's 5/3/1 BBB program in CSV format.
+The plan is based on your 1RM (one-rep max) defined in a setting file.
+
+Usage:
+
+```sh
+$ go run ./cmd/plan/ -h
+Usage of /tmp/go-build3738305972/b001/exe/plan:
+  -bar int
+        bar weight (default 45)
+  -file string
+        workout plan settings file
+  -plates string
+        available plates (default "45,35,25,10,10,5,5,2.5")
+```
+
+Example:
+
+```sh
+$ go run ./cmd/plan/ -file settings.yaml
+Lift;Week;Day;TM %;Weight;Plates;Sets;Reps
+Press;1;1;65%;90;10, 10, 2.5;1;5
+Press;1;1;75%;105;25, 5;1;5
+Press;1;1;85%;115;35;1;5
+Press;1;1;65%;90;10, 10, 2.5;5;5
+Bench;1;1;60%;115;35;5;10
+...
+```
+
+Format of `settings.yaml`:
+
+```yaml
+SquatRepMax: 300
+DeadliftRepMax: 310
+PressRepMax: 145
+BenchRepMax: 205
+```
---
+++ plans/wendler_531_bbb.go
@@ -0,0 +1,119 @@
+package plans
+
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+
+	"github.com/kdeloach/platecalc"
+)
+
+type WorkoutPlanSettings struct {
+	SquatRepMax    int `yaml:"SquatRepMax"`
+	DeadliftRepMax int `yaml:"DeadliftRepMax"`
+	PressRepMax    int `yaml:"PressRepMax"`
+	BenchRepMax    int `yaml:"BenchRepMax"`
+}
+
+type wendler531BBB struct {
+	tree     *platecalc.Node
+	settings *WorkoutPlanSettings
+}
+
+func NewWendler531BBB(tree *platecalc.Node, settings *WorkoutPlanSettings) *wendler531BBB {
+	return &wendler531BBB{
+		tree:     tree,
+		settings: settings,
+	}
+}
+
+func (w *wendler531BBB) Write(writer *csv.Writer) {
+	tms := [][]float32{
+		{.65, .75, .85, .65},
+		{.7, .8, .9, .7},
+		{.75, .85, .95, .75},
+	}
+	sets := [][]int{
+		{1, 1, 1, 5},
+		{1, 1, 1, 5},
+		{1, 1, 1, 5},
+	}
+	reps := [][]int{
+		{5, 5, 5, 5},
+		{3, 3, 3, 5},
+		{5, 3, 1, 5},
+	}
+
+	lifts1 := []string{"Press", "Deadlift", "Bench", "Squat"}
+	lifts2 := []string{"Bench", "Squat", "Press", "Deadlift"}
+
+	// Calculate training max from 90% of 1 rep max
+	weights := map[string]int{
+		"Squat":    platecalc.RoundUpToNearest(float32(w.settings.SquatRepMax)*.9, 5),
+		"Deadlift": platecalc.RoundUpToNearest(float32(w.settings.DeadliftRepMax)*.9, 5),
+		"Press":    platecalc.RoundUpToNearest(float32(w.settings.PressRepMax)*.9, 5),
+		"Bench":    platecalc.RoundUpToNearest(float32(w.settings.BenchRepMax)*.9, 5),
+	}
+
+	// Output header
+	writer.Write([]string{
+		"Lift", "Week", "Day", "TM %", "Weight", "Plates", "Sets", "Reps",
+	})
+	writer.Flush()
+
+	for week := 0; week < 3; week++ {
+		for day := 0; day < 4; day++ {
+			lift1 := lifts1[day]
+			lift2 := lifts2[day]
+
+			w1 := weights[lift1]
+			w2 := weights[lift2]
+
+			// Find optimal sequence of plate changes for 5/3/1 lift
+			setWeights := make([]int, 0)
+			for i := 0; i < 4; i++ {
+				tm := tms[week][i]
+				wt := platecalc.RoundUpToNearest(float32(w1)*tm, 5)
+				setWeights = append(setWeights, wt)
+			}
+
+			plates := platecalc.BestSolution(w.tree, setWeights)
+			if plates == nil {
+				log.Fatalf("no solution found for: %v weight=%v", lift1, w1)
+			}
+
+			// Output rows for 5/3/1 lift
+			for i := 0; i < 4; i++ {
+				s := sets[week][i]
+				r := reps[week][i]
+				tm := tms[week][i]
+				ps := plates[i]
+				wt := setWeights[i]
+				writeRow(writer, lift1, week, day, tm, wt, ps, s, r)
+			}
+
+			// Output rows for 5x10 lift
+			tm := float32(0.6)
+			wt := platecalc.RoundUpToNearest(float32(w2)*tm, 5)
+			ps := platecalc.BestSolution(w.tree, []int{wt})
+			if plates == nil {
+				log.Fatalf("no solution found for: %v weight=%v", lift2, w2)
+			}
+			writeRow(writer, lift2, week, day, tm, wt, ps[0], 5, 10)
+		}
+	}
+}
+
+func writeRow(writer *csv.Writer, lift string, week int, day int, tm float32, wt int, plates *platecalc.Node, sets int, reps int) {
+	writer.Write([]string{
+		lift,
+		fmt.Sprintf("%v", week+1),
+		fmt.Sprintf("%v", day+1),
+		fmt.Sprintf("%v%%", int(tm*100)),
+		fmt.Sprintf("%v", wt),
+		plates.String(),
+		fmt.Sprintf("%v", sets),
+		fmt.Sprintf("%v", reps),
+	})
+	writer.Flush()
+}
